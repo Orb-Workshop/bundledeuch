515976f496ae981b0ede5972af43d4ba18dd3cd5
Benjamin Zaporzan <benzaporzan@gmail.com>
2025-12-29 15:29:28 -0500

    Fixed postinstall script

--- bun_postinstall.ts
+++ bun_postinstall.ts
@@ -4,8 +4,9 @@
   Copies the scriptedeuch map prefabs into ./maps/prefabs/scriptedeuch
 */
 import { cpSync } from "node:fs";
+import * as path from "node:path";
 console.log("Bun - Post-Installation Step");
 console.log("Copying scriptedeuch library prefabs into './maps/prefabs/scriptedeuch' folder...");
-cpSync("../node_modules/scriptedeuch/maps/prefabs/scriptedeuch",
-       "./maps/prefabs/scriptedeuch",
+cpSync(path.join("node_modules", "scriptedeuch", "maps", "prefabs", "scriptedeuch"),
+       path.join("maps", "prefabs", "scriptedeuch"),
        {recursive: true});
